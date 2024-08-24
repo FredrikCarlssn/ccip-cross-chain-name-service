281d6b8b6855423dafab21226364ffcbf17fc70e
FredrikCarlssn <113025364+FredrikCarlssn@users.noreply.github.com>
2024-08-24 21:45:38 +0200

    Done

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,78 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// test/CCIPCrossChainNameServiceTest.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Test} from "forge-std/Test.sol";
+import {IRouterClient, CCIPLocalSimulator, WETH9, LinkToken, BurnMintERC677Helper} from "@chainlink/local/src/ccip/CCIPLocalSimulator.sol";
+import {CrossChainNameServiceRegister} from "../contracts/CrossChainNameServiceRegister.sol";
+import {CrossChainNameServiceReceiver} from "../contracts/CrossChainNameServiceReceiver.sol";
+import {CrossChainNameServiceLookup} from "../contracts/CrossChainNameServiceLookup.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract CCIPCrossChainNameServiceTest is Test {
+    CCIPLocalSimulator public ccipLocalSimulator;
+    CrossChainNameServiceRegister public crossChainNameServiceRegister;
+    CrossChainNameServiceReceiver public crossChainNameServiceReceiver;
+    CrossChainNameServiceLookup public crossChainNameServiceLookup;
+    CrossChainNameServiceLookup public crossChainNameServiceLookup2;
+    address public aliceEOA =
+        address(0xc9c81Af14eC5d7a4Ca19fdC9897054e2d033bf05);
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        // Deploy CCIPLocalSimulator
+        ccipLocalSimulator = new CCIPLocalSimulator();
+
+        // Get Router contract address
+        (
+            uint64 chainSelector,
+            IRouterClient sourceRouter,
+            IRouterClient destinationRouter,
+            WETH9 wrappedNative,
+            LinkToken linkToken,
+            BurnMintERC677Helper ccipBnM,
+            BurnMintERC677Helper ccipLnM
+        ) = ccipLocalSimulator.configuration();
+
+        // Deploy CrossChainNameServiceLookup
+        crossChainNameServiceLookup = new CrossChainNameServiceLookup();
+
+        // Deploy CrossChainNameServiceRegister
+        crossChainNameServiceRegister = new CrossChainNameServiceRegister(
+            address(sourceRouter),
+            address(crossChainNameServiceLookup)
+        );
+
+        crossChainNameServiceLookup.setCrossChainNameServiceAddress(
+            address(crossChainNameServiceRegister)
+        );
+
+        crossChainNameServiceRegister.enableChain(
+            chainSelector,
+            address(destinationRouter),
+            500000
+        );
+
+        crossChainNameServiceLookup2 = new CrossChainNameServiceLookup();
+
+        // Deploy CrossChainNameServiceReceiver
+        crossChainNameServiceReceiver = new CrossChainNameServiceReceiver(
+            address(destinationRouter),
+            address(crossChainNameServiceLookup2),
+            chainSelector
+        );
+
+        crossChainNameServiceLookup2.setCrossChainNameServiceAddress(
+            address(crossChainNameServiceReceiver)
+        );
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+    function test_RegisterAndLookup() public {
+        // Register alice.ccns
+        vm.prank(aliceEOA);
+        crossChainNameServiceRegister.register("alice.ccns");
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        // Lookup alice.ccns
+
+        address result = crossChainNameServiceLookup.lookup("alice.ccns");
+        assertEq(result, aliceEOA);
     }
 }
